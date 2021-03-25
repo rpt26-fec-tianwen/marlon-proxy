7b682a58a0fb91e133d5f926273d0b123c836793
Marlon Esparza <marlonaesparza@gmail.com>
2021-03-24 18:26:48 -0700

    Optimizing performance of server
    -Only writes each bundle.js once, then
    refers to the one written for rest of uptime

--- server/index.js
+++ server/index.js
@@ -11,8 +11,10 @@ const port = 8000;
 const services = {
   card: 'http://localhost:8001/',
   details: 'http://localhost:8002/',
-  related: 'http://localhost:8003/'
-}
+  related: 'http://localhost:8003/',
+  reviews: 'http://localhost:8004/',
+  recieved: false
+};
 
 app.use(cors());
 app.use(express.static(path.resolve('public')));
@@ -23,49 +25,71 @@ app.get('/:id', (req, res) => {
     return;
   }
 
-  return axios.get(`${services.card}${req.params.id}`, {params: {proxy: true}})
-    .then((result) => {
-      const cardBundle = result.data;
-      fs.writeFile(path.resolve(__dirname + '/../public/dist/cardBundle.js'), cardBundle, (error) => {
-        if (error) {
-          throw(error);
-        };
+  if (services.recieved) {
+    console.log('Services have been written, sending...');
+    res.sendFile(path.resolve(__dirname + '/../public/index.html'));
+
+  } else {
+    return axios.get(`${services.card}${req.params.id}`, {params: {proxy: true}})
+      .then((result) => {
+        const cardBundle = result.data;
+        fs.writeFile(path.resolve(__dirname + '/../public/dist/cardBundle.js'), cardBundle, (error) => {
+          if (error) {
+            throw(error);
+          };
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .then(() => {
+        return axios.get(`${services.details}${req.params.id}`, {params: {proxy: true}})
+          .then((result) => {
+            const detailsBundle = result.data;
+            fs.writeFile(path.resolve(__dirname + '/../public/dist/detailsBundle.js'), detailsBundle, (error) => {
+              if (error) {
+                throw(error);
+              };
+            });
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .then(() => {
+        return axios.get(`${services.related}${req.params.id}`, {params: {proxy: true}})
+          .then((result) => {
+            const relatedBundle = result.data;
+            fs.writeFile(path.resolve(__dirname + '/../public/dist/relatedBundle.js'), relatedBundle, (error) => {
+              if (error) {
+                throw(error);
+              };
+            });
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .then(() => {
+        return axios.get(`${services.reviews}bundle`, {params: {proxy: true}})
+          .then((result) => {
+            const reviewsBundle = result.data;
+            fs.writeFile(path.resolve(__dirname + '/../public/dist/reviewsBundle.js'), reviewsBundle, (error) => {
+              if (error) {
+                throw(error);
+              };
+            });
+          })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .then(() => {
+        services.recieved = true;
+        console.log('Successfully wrote services...');
+        res.sendFile(path.resolve(__dirname + '/../public/index.html'));
       });
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-    .then(() => {
-      return axios.get(`${services.details}${req.params.id}`, {params: {proxy: true}})
-        .then((result) => {
-          const detailsBundle = result.data;
-          fs.writeFile(path.resolve(__dirname + '/../public/dist/detailsBundle.js'), detailsBundle, (error) => {
-            if (error) {
-              throw(error);
-            };
-          });
-        });
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-    .then(() => {
-      return axios.get(`${services.related}${req.params.id}`, {params: {proxy: true}})
-        .then((result) => {
-          const relatedBundle = result.data;
-          fs.writeFile(path.resolve(__dirname + '/../public/dist/relatedBundle.js'), relatedBundle, (error) => {
-            if (error) {
-              throw(error);
-            };
-          });
-        });
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-    .then(() => {
-      res.sendFile(path.resolve(__dirname + '/../public/index.html'));
-    });
+  }
 });
 
 app.get('/card/:id', (req, res) => {
@@ -122,6 +146,33 @@ app.get('/related-products/:id', (req, res) => {
     .catch((error) => {
       console.log('PROXY GET /related-products/:id', error);
     });
+});
+
+app.get('/reviews/:id', (req, res) => {
+  const {host, port, path, productId} = req.query;
+  const route = `http://${host}:${port}${path}${productId}`;
+
+  return axios.post(route)
+  .then((result) => {
+    const response = JSON.stringify(result.data.product_overall_rating);
+    res.send(response);
+  })
+  .catch((error) => {
+    console.log('PROXY POST /:id', error);
+  });
+})
+
+app.post('/:id', function (req, res) {
+  const id = req.params.id
+  const route = `${services.reviews}${id}`;
+
+  return axios.post(route)
+  .then((data) => {
+    res.send(data.data)
+  })
+  .catch((error) => {
+    console.log('PROXY POST /:id', error);
+  });
 });
 
 app.listen(port, () => {
